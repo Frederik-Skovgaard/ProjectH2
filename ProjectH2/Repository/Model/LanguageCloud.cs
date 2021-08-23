766b53cb61cb8878e4ebeddacfc69a2a29d13435
Frederik Cornelius Skovgaard <fred56b8@elevcampus.dk>
2021-08-23 15:04:50 +0200

    prop

--- ProjectH2/Repository/Model/LanguageCloud.cs
+++ ProjectH2/Repository/Model/LanguageCloud.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjectH2.Repository.Controller;
 
 namespace ProjectH2.Repository.Model
 {
     class LanguageCloud
     {
+        public Street Street { get; set; }
+        public List<Language> Languages => language.Languages;
+
+        private Language language;
     }
 
     public class Language
     {
-
+        public List<Language> Languages => languages;
+        private List<Language> languages;
     }
 }
