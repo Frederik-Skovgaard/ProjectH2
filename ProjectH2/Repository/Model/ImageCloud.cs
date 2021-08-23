266281a8aa0ead41d61f6e92aa091846481c0324
Frederik Cornelius Skovgaard <fred56b8@elevcampus.dk>
2021-08-23 14:50:51 +0200

    prop

--- ProjectH2/Repository/Model/ImageCloud.cs
+++ ProjectH2/Repository/Model/ImageCloud.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjectH2.Repository.Controller;
 
 namespace ProjectH2.Repository.Model
 {
     class ImageCloud
     {
+       
     }
 
-    class Image
+    class Image : ImageCloud
     {
+        public string Name => name;
+        public string Description => decscription;
+        public string Path => path;
 
+        private string name;
+        private string decscription;
+        private string path;
+
+        public Image(string name_, string description_, string path_)
+        {
+            name = name_;
+            decscription = description_;
+            path = path_;
+        }
     }
 }
