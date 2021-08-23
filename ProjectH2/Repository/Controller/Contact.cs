d4b01142f756cc7d5b0c225455f3579d5f254737
Frederik Cornelius Skovgaard <fred56b8@elevcampus.dk>
2021-08-23 10:03:39 +0200

    Contact propertys & consctrutor

--- ProjectH2/Repository/Controller/Contact.cs
+++ ProjectH2/Repository/Controller/Contact.cs
@@ -8,5 +8,50 @@ namespace ProjectH2.Repository.Controller
 {
     class Contact
     {
+        //Contact name
+        public string FullName => $"{firstName} {lastName}";
+
+        private string firstName;
+        private string lastName;
+
+
+
+        //Address
+        public string Address => $"{street}, {streetNr}";
+
+        private string street;
+        private int streetNr;
+
+
+
+        //Contact information
+        public string Linkedin => linkedin;
+        public string Phone => phone;
+        public string Email => email;
+
+        private string linkedin;
+        private string phone;
+        private string email;
+
+
+
+
+        //Contract construckter
+        public Contact(string fName, string lName, string address, int addressNr, string phoneNr, string emailAddress, string linkedinLink)
+        {
+            //Contact Name
+            firstName = fName;
+            lastName = lName;
+
+            //Address
+            street = address;
+            streetNr = addressNr;
+
+            //Contact information
+            email = emailAddress;
+            phone = phoneNr;
+            linkedin = linkedinLink;
+        }
+        
     }
 }
