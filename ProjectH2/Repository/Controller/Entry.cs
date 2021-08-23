60e735dd67732fafd61c31e064c64097a88fd323
Frederik Cornelius Skovgaard <fred56b8@elevcampus.dk>
2021-08-23 11:46:52 +0200

    Entry name prop

--- ProjectH2/Repository/Controller/Entry.cs
+++ ProjectH2/Repository/Controller/Entry.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjectH2.Repository.Model;
 
 namespace ProjectH2.Repository.Controller
 {
     class Entry
     {
+        //public String Name => file.FullName;
+        private string name;
+        private File file;
 
     }
 
