d270f589d1567af7d1f887a1d966e39e3238292a
Frederik Cornelius Skovgaard <fred56b8@elevcampus.dk>
2021-08-24 08:42:56 +0200

    Tag cloud contrutor and properties

--- ProjectH2/Repository/Model/TagCloud.cs
+++ ProjectH2/Repository/Model/TagCloud.cs
@@ -3,15 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjectH2.Repository.Controller;
 
 namespace ProjectH2.Repository.Model
 {
+    //TODO: Add properties to tag & tag cloud
     class TagCloud
     {
+        //Properties
+        public Street street { get; set; }
+        public List<Tag> TagsList => tag.TagList;
+
+        private Tag tag;
     }
 
     public class Tag
     {
+        //Tag List
+        public List<Tag> TagList => tagList;
+        private List<Tag> tagList;
 
+        //Properties
+        public string Name => name;
+        public string Description => description;
+
+        private string name;
+        private string description;
+
+        /// <summary>
+        /// Constructor for making tags
+        /// </summary>
+        /// <param name="name_"></param>
+        public Tag(string name_)
+        {
+            name = name_;
+        }
     }
 }
