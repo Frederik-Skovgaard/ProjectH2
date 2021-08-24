8fba677fd4275d344c0d0658ebd3fb79aada7f50
Frederik Cornelius Skovgaard <fred56b8@elevcampus.dk>
2021-08-24 15:05:09 +0200

    Formating

--- ProjectH2/Repository/Controller/Entry.cs
+++ ProjectH2/Repository/Controller/Entry.cs
@@ -76,7 +76,7 @@ namespace ProjectH2.Repository.Controller
     //Create a reference
     class Reference : Entry
     {
-
+        
         public Reference(string text, ImageCloud image, FileCloud file, TagCloud tag, LanguageCloud language, bool active)
         {
             Text = text;
@@ -86,6 +86,7 @@ namespace ProjectH2.Repository.Controller
             Language = language;
             Active = active;
         }
+
 
         
     }
